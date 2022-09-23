e11a7bd4559bb339d98d09fe82e07601e03e3a48
ManuelEric31 <manueleric.saputra@gmail.com>
2022-09-23 18:41:52 +0700

    Update Project Tugas 1

--- 19Sept2022/main.cpp
+++ 19Sept2022/main.cpp
@@ -16,15 +16,15 @@ struct paketKiriman
     struct paketKiriman *pointer;
 };
 // Function Prototype
-void pushNilai (struct paketKiriman *p);
-void hapusData (struct paketKiriman *p ,int index);
-void popDataTerakhir (struct paketKiriman*p);
-void displayNilai (struct paketKiriman display);
+void pushNilai(struct paketKiriman *p);
+void hapusData(struct paketKiriman *p, int index);
+void popDataTerakhir(struct paketKiriman *p);
+void displayNilai(struct paketKiriman display);
 
 // Function
-void pushNilai (struct paketKiriman *p)
+void pushNilai(struct paketKiriman *p)
 {
-    
+
     if (i == 0)
     {
         p->pointer = new struct paketKiriman[maxValue];
@@ -40,74 +40,75 @@ void pushNilai (struct paketKiriman *p)
     top++;
 }
 
-void hapusData (struct paketKiriman *p ,int index)
+void hapusData(struct paketKiriman *p, int index)
 {
     if (index >= 0 && index < totalBarang)
     {
-        for (int j = index; j < totalBarang-1; j++)
-            p->pointer[j] = p->pointer[j+1];
+        for (int j = index; j < totalBarang - 1; j++)
+            p->pointer[j] = p->pointer[j + 1];
         totalBarang--;
         top--;
     }
 }
 
-void popDataTerakhir (struct paketKiriman*p)
+void popDataTerakhir(struct paketKiriman *p)
 {
     totalBarang--;
     top--;
     displayNilai(*p);
 }
 
-void displayNilai (struct paketKiriman display)
+void displayNilai(struct paketKiriman display)
 {
     cout << "\nHasil data : " << endl;
     for (int j = 0; j < totalBarang; j++)
     {
-        cout << "Data ke-" << j+1 << "\n" << display.pointer[j].jenisBarang << " "
-                << display.pointer[j].beratBarang << " "
-                << display.pointer[j].kotaTujuan << endl;
+        cout << "Data ke-" << j + 1 << "\n"
+             << display.pointer[j].jenisBarang << " "
+             << display.pointer[j].beratBarang << " "
+             << display.pointer[j].kotaTujuan << endl;
     }
 }
 
+int main()
+{
 
-int main () {
-    
     int pilih{}; // indexHapus{};
     struct paketKiriman data;
-    cout << "Limit Stack Data hanyalah : " << maxValue << endl; 
+    cout << "Limit Stack Data hanyalah : " << maxValue << endl;
     do
     {
-            cout << "\n1. Tambah Data : " << endl;
-            cout << "2. Tampilkan Data : " << endl;
-            cout << "3. Hapus Data Terakhir : " << endl;
-            cout << "4. Keluar Program : " << endl;
-            
-            cout << "\nMasukkan pilihan anda : ";
-            cin >> pilih;
-            
-            if (pilih == 1)
+        cout << "\n1. Tambah Data : " << endl;
+        cout << "2. Tampilkan Data : " << endl;
+        cout << "3. Hapus Data Terakhir : " << endl;
+        cout << "4. Keluar Program : " << endl;
+
+        cout << "\nMasukkan pilihan anda : ";
+        cin >> pilih;
+
+        if (pilih == 1)
+        {
+            if (top < maxValue)
+                pushNilai(&data);
+            else
+                cout << "Stack is FULL!!!" << endl;
+        }
+        else if (pilih == 2)
+            displayNilai(data);
+        else if (pilih == 3)
+        {
+            if (top > 0)
             {
-                if (top < maxValue)
-                    pushNilai (&data);
-                else 
-                    cout << "Stack is FULL!!!" << endl;
-            }
-            else if (pilih == 2)
-                displayNilai(data);
-            else if (pilih == 3)
-            {
-                 if (top > 0)
-                {
-//                    cin >> indexHapus;
-//                    hapusData(&data,indexHapus);
-                        popDataTerakhir(&data);
-                }
-                 else
-                     cout << "Stack is EMPTY!!!" << endl;
+                //                    cin >> indexHapus;
+                //                    hapusData(&data,indexHapus);
+                popDataTerakhir(&data);
             }
             else
-                exit(1);
-    } while (pilih >= 1 || pilih <=3 );
-    
+                cout << "Stack is EMPTY!!!" << endl;
+        }
+        else
+            exit(1);
+    } while (pilih >= 1 || pilih <= 3);
+
     return 0;
 }
