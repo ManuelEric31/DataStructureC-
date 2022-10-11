7111a63326819035a7902c55fb06d369f1a23484
ManuelEric31 <manueleric.saputra@gmail.com>
2022-10-11 13:00:44 +0700

    Update Tugas

--- 4Oct2022/main.cpp
+++ 4Oct2022/main.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 struct TNode{
-    int data;
+    int NilaiX;
+    int NilaiY;
     TNode *next;
 };
 
@@ -17,6 +18,17 @@ void init (){
     tail = NULL;
 }
 
+int countNodes (struct TNode *p)
+{
+    int count{};
+    while (p != NULL)
+    {
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
 int isEmpty(){
     if(tail==NULL)
         return 1;
@@ -24,11 +36,12 @@ int isEmpty(){
         return 0;
 }
 
-void insertDepan(int databaru)
+void insertDepan(int databaru1, int databaru2)
 {
     TNode *baru;
     baru = new TNode;
-    baru->data = databaru;
+    baru->NilaiX = databaru1;
+    baru->NilaiY = databaru2;
     baru->next = NULL;
     
     if(isEmpty()==1)
@@ -48,7 +61,7 @@ void tampil(){
         {
             while(bantu!=NULL)
             {
-                cout<<bantu->data<<" ";
+                cout<<bantu->NilaiX<<" " << bantu->NilaiY << endl;
                 bantu=bantu->next;
             }
         }
@@ -58,34 +71,65 @@ void tampil(){
 
 void hapusDepan()
 {
-
     TNode *hapus;
-    int d;
+    int delete1, delete2;
     if (isEmpty()==0)
     {
         if(head!=tail)
         {
             hapus=head;
-            d=hapus->data;
+            delete1=hapus->NilaiX;
+            delete2 = hapus->NilaiY;
             head=head->next;
             delete hapus;
         }
         else
         {
-            d=tail->data;
+            delete1=tail->NilaiX;
+            delete2=tail->NilaiY;
             head=tail=NULL;
         }
-    cout<<d<<" Terhapus";
+    cout<<delete1 << " dan " << delete2 << " Terhapus";
     }
     else 
         cout<<"Data Masih kosong\n";
 }
 
-void insertBelakang (int data)
+void hapusBelakang()
+{
+    TNode *bantu = tail;
+    TNode *hapus = head;
+    int delete1, delete2;
+    if (isEmpty()==0)
+    {
+        for (int i = 0; i < countNodes(head)-2; i++)
+            hapus = hapus->next;
+        if(hapus->next != NULL)
+        {
+            delete1=tail->NilaiX;
+            delete2 = tail->NilaiY;
+            tail = hapus;
+            tail->next = NULL;
+            delete bantu;
+        }
+        else
+        {
+            delete1=tail->NilaiX;
+            delete2=tail->NilaiY;
+            delete bantu;
+            head=tail=NULL;
+        }
+            cout<<delete1 << " dan " << delete2 << " Terhapus";
+    }
+    else 
+        cout<<"Data Masih kosong\n";
+}
+void insertBelakang (int data1, int data2)
 {
     struct TNode *baru;
     baru = new struct TNode;
-    baru->data = data;
+    baru->NilaiX = data1;
+    baru->NilaiY = data2;
     baru->next = NULL;
     if (isEmpty())
         head = tail = baru;
@@ -97,19 +141,21 @@ void insertBelakang (int data)
     cout<<"Data masuk\n";
 }
 
-void searchData (int key)
+void searchData (int key1, int key2)
 {
-    int flag {-1}; 
+    int flag1 {-1}; 
+    int flag2 {-1}; 
     struct TNode *bantu;
     bantu = head;
     if (!isEmpty())
     {
         while (bantu != NULL)
         {
-            if (bantu->data == key)
+            if (bantu->NilaiX == key1 && bantu->NilaiY == key2)
             {
-                flag =  key;
-                cout << "Data ditemukan : " << flag << endl;
+                flag1 =  key1;
+                flag2 =  key2;
+                cout << "Data ditemukan : " << flag1 << " dan "<< flag2 <<endl;
                 break;
             }
             else
@@ -121,27 +167,69 @@ void searchData (int key)
         cout << "Linked List masih kosong" << endl;
 }
 
-void insertAfter (int data)
+void insertAfter (int data1, int data2)
 {
     struct TNode *bantu = head;
-    int cari{};
+    int cari1{}, cari2{};
     cout << "Insert data setelah nilai : ";
-    cin >> cari;
+    cin >> cari1 >> cari2;
     if (!isEmpty())
     {
         struct TNode *baru;
         baru = new struct TNode;
-        baru->data = data;
+        baru->NilaiX = data1;
+        baru->NilaiY = data2;
         baru->next = NULL;
         
         while (bantu != NULL)
         {
-            if (bantu->data == cari)
+            if (bantu->NilaiX == cari1 && bantu->NilaiY == cari2)
             {
-                baru->next = bantu->next;
-                bantu->next = baru;
+                if (bantu->next != NULL)
+                {
+                    baru->next = bantu->next;
+                    bantu->next = baru;
+                }
+                else
+                {
+                    bantu->next = baru;
+                    tail = baru;
+                }
             }
             bantu = bantu->next;
+        }
+    }
+    else
+        cout << "Data tidak ditemukan" << endl;
+}
+
+void insertBefore (int data1, int data2)
+{
+    struct TNode *bantu = head;
+    int cari1{}, cari2{};
+    cout << "Insert data sebelum nilai : ";
+    cin >> cari1 >> cari2;
+    if (!isEmpty())
+    {
+        struct TNode *baru;
+        baru = new struct TNode;
+        baru->NilaiX = data1;
+        baru->NilaiY = data2;
+        baru->next = NULL;
+        
+        while (bantu != NULL)
+        {
+            if (bantu->next->NilaiX == cari1 && bantu->next->NilaiY == cari2)
+            {
+                    baru->next = bantu->next;
+                    bantu->next = baru;
+            }
+            else
+            {
+                baru->next = head;
+                head = baru;
+            }
+            break;
         }
     }
     else
@@ -163,7 +251,7 @@ void clear()
 
 int main()
 {
-    int pil{}, databaru{}, cari{};
+    int pil{}, databaru1{},databaru2{};
     do
     {
         system("cls");
@@ -174,58 +262,70 @@ int main()
         cout<<" = 1. Insert Depan          ="<<endl;
         cout<<" = 2. Insert Belakang       ="<<endl;
         cout<<" = 3. Insert After          ="<<endl;
-        cout<<" = 4. Delete Depan          ="<<endl;
-        cout<<" = 5. Tampil Data           ="<<endl;
-        cout<<" = 6. Cari Data             ="<<endl;
-        cout<<" = 7. Clear                 ="<<endl;
-        cout<<" = 8. Exit                  ="<<endl;
+        cout<<" = 4. Insert Before         ="<<endl;
+        cout<<" = 5. Delete Depan          ="<<endl;
+        cout<<" = 6. Delete Belakang       ="<<endl;
+        cout<<" = 7. Tampil Data           ="<<endl;
+        cout<<" = 8. Cari Data             ="<<endl;
+        cout<<" = 9. Clear                 ="<<endl;
+        cout<<" = 10. Exit                 ="<<endl;
         cout<<" ============================"<<endl;
         cout<<" Masukan Pilihan : ";cin>>pil;
         switch (pil)
         {
             case 1: system("cls");{
                 cout<<"Masukan Data = ";
-                cin>>databaru;
-                insertDepan(databaru);
+                cin>>databaru1 >> databaru2 ;
+                insertDepan(databaru1, databaru2);
                 break;
             }
             case 2: system("cls");{
                 cout<<"Masukan Data = ";
-                cin>>databaru;
-                insertBelakang(databaru);
+                cin>>databaru1 >> databaru2;
+                insertBelakang(databaru1, databaru2);
                 break;
             }
             case 3: system("cls");{
                 cout<<"Masukan Data = ";
-                cin>>databaru;
-                insertAfter(databaru);
+                cin>>databaru1 >> databaru2;
+                insertAfter(databaru1, databaru2);
                 break;
             }
             case 4: system("cls");{
-                hapusDepan();
+                cout<<"Masukan Data = ";
+                cin>>databaru1 >> databaru2;
+                insertBefore(databaru1, databaru2);
                 break;
             }
             case 5: system("cls");{
+                hapusDepan();
+                break;
+            }
+            case 6: system("cls");{
+                hapusBelakang();
+                break;
+            }
+            case 7: system("cls");{
                 tampil();
                 break;
             }
-            case 6: system("cls");
+            case 8: system("cls");
             {
                 if (!isEmpty())
                 {
                     cout<<"Cari Data = ";
-                    cin>>databaru;
-                    searchData(cari);
+                    cin>>databaru1 >> databaru2;
+                    searchData(databaru1, databaru2);
                 }
                 else
                     cout << "Data masih kosong" << endl;
                 break;
             }
-            case 7: system("cls");{
+            case 9: system("cls");{
                 clear();
                 break;
             }
-            case 8: system("cls");{
+            case 10: system("cls");{
                 return 0;
                 break;
             }
@@ -236,6 +336,6 @@ int main()
         }
         getch();
     }
-    while (pil!=7);
+    while (pil!=10);
     return 0;
 }
